ad100ad5ba75ca79b7e5ba735ab9f21ab0e96680
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 10:03:45 +0200

    submenu on books works

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,7 +1,7 @@
 import { BookGroupFantasy } from '../components/BookGroupFantasy';
 import { BookGroupPolitics } from '../components/BookGroupPolitics';
 import { BookGroupTechnology } from '../components/BookGroupTechnology';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 export const PageBooks = () => {
 	return (
@@ -10,14 +10,22 @@ export const PageBooks = () => {
 			<p>This is the book page.</p>
 
 			<ul>
-				<li><NavLink to="fantasy">Fantasy</NavLink></li>
-				<li><NavLink to="politics">Politics</NavLink></li>
-				<li><NavLink to="technology">Technology</NavLink></li>
+				<li>
+					<NavLink to="fantasy">Fantasy</NavLink>
+				</li>
+				<li>
+					<NavLink to="politics">Politics</NavLink>
+				</li>
+				<li>
+					<NavLink to="technology">Technology</NavLink>
+				</li>
 			</ul>
 
-			<BookGroupFantasy />
-			<BookGroupPolitics />
-			<BookGroupTechnology />
+			<Routes>
+				<Route path="fantasy" element={<BookGroupFantasy />}/>
+				<Route path="politics" element={<BookGroupPolitics />}/>
+				<Route path="technology" element={<BookGroupTechnology />}/>
+			</Routes>
 		</div>
 	);
 };
