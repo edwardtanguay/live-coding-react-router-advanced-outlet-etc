b5fa7ba95c5366991fddc91fa0f8135662015548
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 09:57:40 +0200

    book navlinks work

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,15 +1,23 @@
 import { BookGroupFantasy } from '../components/BookGroupFantasy';
 import { BookGroupPolitics } from '../components/BookGroupPolitics';
 import { BookGroupTechnology } from '../components/BookGroupTechnology';
+import { NavLink } from 'react-router-dom';
 
 export const PageBooks = () => {
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
 			<p>This is the book page.</p>
-			<BookGroupFantasy/>
-			<BookGroupPolitics/>
-			<BookGroupTechnology/>
+
+			<ul>
+				<li><NavLink to="fantasy">Fantasy</NavLink></li>
+				<li><NavLink to="politics">Politics</NavLink></li>
+				<li><NavLink to="technology">Technology</NavLink></li>
+			</ul>
+
+			<BookGroupFantasy />
+			<BookGroupPolitics />
+			<BookGroupTechnology />
 		</div>
-	)
-}
+	);
+};
