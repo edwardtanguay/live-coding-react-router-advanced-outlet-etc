81a41f43887ccd0000cf9f4dea5cb1b2d77d7e7f
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 10:14:40 +0200

    basic howtos page works

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { PageWelcome } from './pages/PageWelcome';
 import { PageBooks } from './pages/PageBooks';
+import { PageHowtos } from './pages/PageHowtos';
 import { PageAbout } from './pages/PageAbout';
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 
@@ -11,11 +12,13 @@ function App() {
 			<hr />
 			<NavLink to="/welcome">Welcome</NavLink> |{' '}
 			<NavLink to="/books">Books</NavLink> |{' '}
+			<NavLink to="/howtos">Howtos</NavLink> |{' '}
 			<NavLink to="/about">About</NavLink>
 			<hr />
 			<Routes>
 				<Route path="/welcome" element={<PageWelcome />} />
 				<Route path="/books/*" element={<PageBooks />} />
+				<Route path="/howtos" element={<PageHowtos />} />
 				<Route path="/about" element={<PageAbout />} />
 				<Route path="/" element={<Navigate to="/welcome" replace />}/>
 			</Routes>
---
+++ src/pages/PageHowtos.jsx
@@ -0,0 +1,8 @@
+export const PageHowtos = () => {
+	return (
+		<>
+			<h2>Howtos</h2>
+			<p>These are my howtos:</p>
+		</>
+	);
+};
