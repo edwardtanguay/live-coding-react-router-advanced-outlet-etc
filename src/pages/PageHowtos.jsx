7f60b93f5386d925c8ac401d3dad3982b25fcbda
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 10:21:09 +0200

    howtos are loading

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -6,10 +6,20 @@ const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
 export const PageHowtos = () => {
 	const [howtos, setHowtos] = useState([]);
 
+	useEffect(() => {
+		(async () => {
+			setHowtos((await axios.get(url)).data);
+		})();
+	}, []);
+
 	return (
 		<>
 			<h2>Howtos</h2>
-			<p>I currently have {howtos.length} howtos:</p>
+			{howtos.length === 0 ? (
+				<div className="loading">Loading...</div>
+			) : (
+				<p>I currently have {howtos.length} howtos:</p>
+			)}
 		</>
 	);
 };
