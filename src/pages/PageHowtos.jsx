40c292ae3c30e10a0c315b2e5ce60e0c72fc1ed9
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 10:18:47 +0200

    0 howtos

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -1,8 +1,15 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
+
 export const PageHowtos = () => {
+	const [howtos, setHowtos] = useState([]);
+
 	return (
 		<>
 			<h2>Howtos</h2>
-			<p>These are my howtos:</p>
+			<p>I currently have {howtos.length} howtos:</p>
 		</>
 	);
 };
