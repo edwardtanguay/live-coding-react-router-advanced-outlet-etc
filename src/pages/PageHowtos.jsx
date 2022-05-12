dde8628a5b9956a93604d339399046757e2f4406
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-12 11:56:33 +0200

    finished

--- src/pages/PageHowtos.jsx
+++ src/pages/PageHowtos.jsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, NavLink, useNavigate, Outlet } from 'react-router-dom';
+import {
+	useParams,
+	NavLink,
+	useNavigate,
+	Outlet,
+	useLocation,
+} from 'react-router-dom';
 
 const url = 'https://edwardtanguay.netlify.app/share/howtos.json';
-const currentCategory = 'React';
+const currentCategory = 'PHP';
 
 export const PageHowtos = () => {
 	const [howtos, setHowtos] = useState([]);
 	let { id } = useParams();
 	id = Number(id);
 	const navigate = useNavigate();
+	const location = useLocation();
+	console.log(location.pathname);
 
 	useEffect(() => {
 		(async () => {
@@ -25,12 +33,17 @@ export const PageHowtos = () => {
 	const getCurrentHowto = () => {
 		if (id) {
 			return howtos.find((m) => m.id == id);
-		} else {
-			if (howtos.length > 0) {
-				const _id = String(howtos[0].id);
-				navigate(_id);
-			}
 		}
+		// else {
+		// 	if (howtos.length > 0) {
+		// 		const _id = String(howtos[0].id);
+		// 		navigate(_id);
+		// 	}
+		// }
+	};
+
+	const handleCloseButton = () => {
+		navigate('/howtos');
 	};
 
 	return (
@@ -45,22 +58,31 @@ export const PageHowtos = () => {
 						howtos:
 					</p>
 
-					<nav>
-						<ul>
-							{howtos.map((howto, index) => {
-								return (
-									<li key={index}>
-										<NavLink to={`${howto.id}`}>
-											{howto.title}
-										</NavLink>
-									</li>
-								);
-							})}
-						</ul>
-					</nav>
-					<hr />
+					{location.pathname === '/howtos' ? (
+						<nav>
+							<ul>
+								{howtos.map((howto, index) => {
+									return (
+										<li key={index}>
+											<NavLink to={`${howto.id}`}>
+												{howto.title}
+											</NavLink>
+										</li>
+									);
+								})}
+							</ul>
+						</nav>
+					) : (
+						<div className="buttonArea">
+							<button
+								className="close"
+								onClick={handleCloseButton}
+							>
+								close
+							</button>
+						</div>
+					)}
 					<Outlet context={getCurrentHowto()} />
-					<hr />
 				</>
 			)}
 		</div>
